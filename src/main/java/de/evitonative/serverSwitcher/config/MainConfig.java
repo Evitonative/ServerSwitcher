ae17b2b37bac3275502076cfbae6784a1579a2da
Evitonative <47825249+Evitonative@users.noreply.github.com>
2025-09-25 22:35:20 +0200

    remove unused constructors

--- src/main/java/de/evitonative/serverSwitcher/config/MainConfig.java
+++ src/main/java/de/evitonative/serverSwitcher/config/MainConfig.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 
 public class MainConfig {
+    @SuppressWarnings("unused")
     public int configVersion;
     public Boolean disablePing;
     public Integer pingTimeoutMs;
@@ -11,21 +12,6 @@ public class MainConfig {
     public LinkedHashMap<String, ServerGroup> groups; // todo: toml4j does not preserve order in the config file, so it should probably be replace with something that does
     public LinkedHashMap<String, ServerDetails> servers;
     private Config configHandler;
-
-    public MainConfig(int configVersion,
-                      Boolean disablePing,
-                      Integer pingTimeoutMs,
-                      Format format,
-                      LinkedHashMap<String, ServerGroup> groups,
-                      LinkedHashMap<String, ServerDetails> servers
-    ) {
-        this.configVersion = configVersion;
-        this.disablePing = disablePing;
-        this.pingTimeoutMs = pingTimeoutMs;
-        this.format = format;
-        this.groups = groups;
-        this.servers = servers;
-    }
 
     public void setConfigHandler(Config config) {
         this.configHandler = config;
@@ -45,29 +31,9 @@ public class MainConfig {
         public String serverAccessDenied;
         public String onlyPlayers;
         public String placeholderFallback;
-
-        public Format(String messageHeading,
-                      String groupHeading,
-                      String serverNameWrapper,
-                      String unavailableServerNameWrapper,
-                      String serverSeparator,
-                      String serverHoverText,
-                      String serverAccessDenied,
-                      String onlyPlayers,
-                      String placeholderFallback
-        ) {
-            this.messageHeading = messageHeading;
-            this.groupHeading = groupHeading;
-            this.serverNameWrapper = serverNameWrapper;
-            this.unavailableServerNameWrapper = unavailableServerNameWrapper;
-            this.serverSeparator = serverSeparator;
-            this.serverHoverText = serverHoverText;
-            this.serverAccessDenied = serverAccessDenied;
-            this.onlyPlayers = onlyPlayers;
-            this.placeholderFallback = placeholderFallback;
-        }
     }
 
+    @SuppressWarnings("CanBeFinal")
     public static final class ServerDetails {
         public String friendlyName;
         public String group;
@@ -86,13 +52,5 @@ public class MainConfig {
     public static class ServerGroup {
         public String friendlyName;
         public Boolean restricted;
-
-        public ServerGroup(
-                String friendlyName,
-                Boolean restricted
-        ) {
-            this.friendlyName = friendlyName;
-            this.restricted = restricted;
-        }
     }
 }
